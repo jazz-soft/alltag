4e6e56efcb92f0c9a2090840ce7782e75c50794a
Sema <sema@pyva.net>
2019-11-18 20:58:42 -0500

    Added tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # alltag
+[![npm](https://img.shields.io/npm/v/alltag.svg)](https://www.npmjs.com/package/alltag)
+
 Coming soon
---
+++ test/mocha.js
@@ -0,0 +1,20 @@
+﻿var assert = require('assert');
+var alltag = require('..');
+
+describe('alltag parser', function() {
+  it('and', function() {
+    var x = alltag.parse(' a b ');
+    var y = [ 'and', [ 'tag', 'a' ], [ 'tag', 'b' ] ];
+    assert.equal(JSON.stringify(x), JSON.stringify(y));
+  });
+  it('or', function() {
+    var x = alltag.parse(' a, b ');
+    var y = [ 'or', [ 'tag', 'a' ], [ 'tag', 'b' ] ];
+    assert.equal(JSON.stringify(x), JSON.stringify(y));
+  });
+  it('not', function() {
+    var x = alltag.parse(' ! !!a ');
+    var y = [ 'not', [ 'tag', 'a' ] ];
+    assert.equal(JSON.stringify(x), JSON.stringify(y));
+  });
+});
