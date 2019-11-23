5de385f983118cb069fda79fcb2b5b3a5b2a2747
Sema <sema@pyva.net>
2019-11-22 23:45:12 -0500

    Coverage

--- test/mocha.js
+++ test/mocha.js
@@ -2,19 +2,33 @@
 var alltag = require('..');
 
 describe('alltag parser', function() {
-  it('and', function() {
+  it('a b', function() {
     var x = alltag.parse(' a b ');
     var y = [ 'and', [ 'tag', 'a' ], [ 'tag', 'b' ] ];
     assert.equal(JSON.stringify(x), JSON.stringify(y));
   });
-  it('or', function() {
+  it('a, b', function() {
     var x = alltag.parse(' a, b ');
     var y = [ 'or', [ 'tag', 'a' ], [ 'tag', 'b' ] ];
     assert.equal(JSON.stringify(x), JSON.stringify(y));
   });
-  it('not', function() {
+  it('!a', function() {
     var x = alltag.parse(' ! !!a ');
     var y = [ 'not', [ 'tag', 'a' ] ];
     assert.equal(JSON.stringify(x), JSON.stringify(y));
+  });
+  it('a (b, c))', function() {
+    var x = alltag.parse('a (b, c)');
+    var y = [ 'and', [ 'tag', 'a' ], [ 'or', [ 'tag', 'b' ], [ 'tag', 'c' ] ] ];
+    assert.equal(JSON.stringify(x), JSON.stringify(y));
+  });
+  it('% - throws', function() {
+    assert.throws(function() { alltag.parse('%'); });
+  });
+  it('( a - throws', function() {
+    assert.throws(function() { alltag.parse('( a'); });
+  });
+  it('a ) - throws', function() {
+    assert.throws(function() { alltag.parse('a )'); });
   });
 });
